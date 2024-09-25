f640d2db63ec32dc57237c3568e6321a5a863b72
Om Patel <ompatel2019@gmail.com>
2024-09-25 22:21:45 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div class="font-sans text-lg">
+        This text uses the DM Sans font.
+      </div>
+    </div>
+  )
+}
+
+export default App
