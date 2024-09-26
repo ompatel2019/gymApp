d9ce7ddf02388aaf2073a85ee6ed8ed7b4c86335
Om Patel <ompatel2019@gmail.com>
2024-09-26 12:01:30 +1000

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="flex flex-col justify-center items-center responsivePad min-h-screen space-y-4">
+      <p className="font-bold p">IT'S TIME TO GET</p>
+
+      <h1 className="h1 font-bold text-blue-500">MASSIVE</h1>
+
+      <div className='flex flex-col items-center space-y-6 2xl:space-y-8'>
+        <p className="text-center h7 font-normal">I acknowledge the potential risks associated with significant muscle growth, including body dysmorphia and challenges with everyday mobility.</p>
+        <button className='Button'>
+          Start the journey
+        </button>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily:{
-        sans:["DM Sans", 'sans-serif'],
+        sans: ["DM Sans", 'sans-serif'],
       },
     },
   },
