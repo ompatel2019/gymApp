6b6217d4b356852e8a3fe21badb23fe3387e2676
Om Patel <ompatel2019@gmail.com>
2024-09-27 22:27:16 +1000

    final changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,11 @@ const Hero = () => {
         IT'S TIME TO GET
       </p>
 
-      <h1 className="h1 font-bold text-blue-500">
+      <h1 className="h1 font-black text-blue-500">
         MASSIVE
       </h1>
 
       <div className="flex flex-col items-center space-y-6 2xl:space-y-8">
-        <p className="h7 text-center font-normal">
-          I acknowledge the potential risks associated with significant muscle growth, including body dysmorphia and challenges with everyday mobility.
-        </p>
-        
         <a href="#generator">
           <button className="button">
             Start the journey
